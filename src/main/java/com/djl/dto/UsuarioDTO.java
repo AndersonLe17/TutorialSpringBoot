c0656f6a3b0d61ba23def816470f6dc82c808bfb
AndersonLe17 <anderleorellana@gmail.com>
2024-01-17 20:43:28 -0500

    Primer commit

---
+++ src/main/java/com/djl/dto/UsuarioDTO.java
@@ -0,0 +1,18 @@
+package com.djl.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class UsuarioDTO {
+    private Integer id;
+    private String nombre;
+    private String correo;
+    private Date fechaNacimiento;
+    private String celular;
+}
